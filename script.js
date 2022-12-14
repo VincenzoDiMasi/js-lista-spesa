0258b7d216f38a988fd2ac03ec9a3fde8aac00f2
VincenzoDiMasi <v.dimasi93@gmail.com>
2022-12-14 15:57:23 +0100

    Finish

--- script.js
+++ script.js
@@ -2,3 +2,28 @@ console.log('JS OK')
 
 /*Data una lista della spesa (inventatela), stampare in pagina
  gli elementi della lista individualmente con un ciclo while.*/
+
+ const list = ['sale', 'pepe', 'olio'];
+
+ 
+ let i = 0;
+ 
+ // Creo un ciclo while
+ while (i < list.length) {
+
+    
+   // Recupero l'elemento dalla lista
+   const listElements = list[i];
+ 
+   // Creo un <li> con gli elementi nell'array
+   const li = document.createElement('li');
+   li.innerText = listElements;
+ 
+   // Aggiungo <li> alla lista <ul> nell'HTML
+   const ul = document.querySelector('ul');
+   ul.appendChild(li);
+
+   i++;
+ 
+ }
+ 
